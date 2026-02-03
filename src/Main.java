1248c39ee6c23a020eea0a6dbcc06aa009455257
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2026-02-03 10:16:21 -0800

    adding

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int num[] = {1,3,5,7,9};
+        int num[][] = {{1,3,5,7,9},{11,13,15,17,19},{21,23,25,27,29}};
         Scanner scan = new Scanner(System.in);
-        System.out.println("input any number");
-        int i = scan.nextInt();
+        System.out.println("input the 10th");
+        int i10 = scan.nextInt();
+        System.out.println("input the 1th");
+        int i1 = scan.nextInt();
         int total = 0;
-        for(int e=0; e<=i-1; e++)
-            total=total+num[e];
+        for(int e10=0; e10<=i10; e10++)
+            for(int e1=0;e1<=i1-1;e1++)
+                total=total+num[e10][e1];
+
         System.out.println(total);
     }
 }
