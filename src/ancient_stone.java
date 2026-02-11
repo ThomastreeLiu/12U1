cef8dc12fce2f6b32720c24512afa6a0bf59da7c
Thomastree <thomastree@126.com>
2026-02-11 08:59:09 -0800

    final

--- src/ancient_stone.java
+++ src/ancient_stone.java
@@ -18,37 +18,37 @@ public class ancient_stone extends JPanel implements ActionListener {
     public ancient_stone() {
         setBackground(Color.BLUE);
         JLabel pic = new JLabel(createImageIcon("stone_quest.jpg"));
-        JLabel txt = new JLabel("choose the correct one fit the graph");
+        JLabel txt = new JLabel("choose the correct one fit the graph\n");
         txt.setForeground(Color.WHITE);
        
        	stone1.setBackground(Color.WHITE);
         stone1.setForeground(Color.BLUE);
-	ImageIcon icon = new ImageIcon("stone1.png");
-	stone1.setIcon(icon);
+	ImageIcon icon1 = new ImageIcon("stone1.jpg");
+	stone1.setIcon(icon1);
         stone1.setOpaque(true);
         stone1.addActionListener(this);
        	stone1.setActionCommand("Guess1");
        
         stone2.setBackground(Color.WHITE);
         stone2.setForeground(Color.BLUE);
-	ImageIcon icon = new ImageIcon("stone2.png");
-	stone2.setIcon(icon);
+	ImageIcon icon2 = new ImageIcon("stone2.jpg");
+	stone2.setIcon(icon2);
         stone2.setOpaque(true);
         stone2.addActionListener(this);
         stone2.setActionCommand("Guess2");
        
        	stone3.setBackground(Color.WHITE);
        	stone3.setForeground(Color.BLUE);
-	ImageIcon icon = new ImageIcon("stone3.png");
-	stone3.setIcon(icon);
+	ImageIcon icon3 = new ImageIcon("stone3.jpg");
+	stone3.setIcon(icon3);
        	stone3.setOpaque(true);
         stone3.addActionListener(this);
         stone3.setActionCommand("Guess3");
        
        	stone4.setBackground(Color.WHITE);
         stone4.setForeground(Color.BLUE);
-	ImageIcon icon = new ImageIcon("stone4.png");
-	stone4.setIcon(icon);
+	ImageIcon icon4 = new ImageIcon("stone4.jpg");
+	stone4.setIcon(icon4);
         stone4.setOpaque(true);
         stone4.addActionListener(this);
         stone4.setActionCommand("Guess4");
@@ -65,10 +65,10 @@ public class ancient_stone extends JPanel implements ActionListener {
     }
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand ().equals ("Guess4")){
-       		JOptionPane.showMessageDialog(null,"Title","You won, it's the correct picture.",JOptionPane.INFORMATION_MESSAGE); 
+       		JOptionPane.showMessageDialog(null,"You won, this is the correct picture","Lose",JOptionPane.INFORMATION_MESSAGE); 
        	}
 	else 
-		JOptionPane.showMessageDialog(null,"Title","Nope, try again.",JOptionPane.INFORMATION_MESSAGE); 
+		JOptionPane.showMessageDialog(null,"Nope, try again","Lose",JOptionPane.INFORMATION_MESSAGE); 
     }
     protected static ImageIcon createImageIcon(String path) {
         java.net.URL imgURL = ancient_stone.class.getResource(path);
@@ -83,7 +83,7 @@ public class ancient_stone extends JPanel implements ActionListener {
     public static void main(String[] args) {
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("EyeballBlizzard");
-        frame.setSize(360, 470);         //resizes JFrame pane size
+        frame.setSize(640, 900);         //resizes JFrame pane size
         frame.setBackground(Color.WHITE);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ancient_stone panel = new ancient_stone();
