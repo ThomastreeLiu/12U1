e2dffb676a87f3cf80ba06c54d3a111f3d9e62e6
Thomastree <thomastree@126.com>
2026-02-26 13:33:31 -0800

    phoneNumbers finish

--- src/phoneNumbers.java
+++ src/phoneNumbers.java
@@ -15,12 +15,12 @@ public class phoneNumbers{
     public phoneNumbers (){   
 
      
-             //123456789abcde
+             //0123456789abcd
 String BCSS = "(604) 713-8180";   
 
 System.out.println (BCSS.substring (0, 14));           
 
- 
+
 
    	System.out.println(BCSS.substring(1,4));
 
@@ -39,34 +39,54 @@ System.out.println (BCSS.substring (0, 14));
         //(f) 7   
 	System.out.println(BCSS.charAt(6));
 
-	//blocker
+	//mark
 	             //0123456789abcd
 		     //(604) 713-8180
-        //(g) 716-8180   
-	System.out.println(BCSS.substring(6,8));
-	System.out.print(BCSS.charAt(2));
+        //(g) 716-8180 
+             //0123456789abcd
+//	BCSS = "(604) 713-8180";   
+
+	  
+	System.out.println();
+	System.out.print(BCSS.substring(6,8));
+	System.out.print(BCSS.charAt(1));
 	System.out.print(BCSS.substring(9,14));
 
         //(h) 6047138180   
-	System.out.println(BCSS.substring(1,4));
+	System.out.println();
+	System.out.print(BCSS.substring(1,4));
 	System.out.print(BCSS.substring(6,9));
 	System.out.print(BCSS.substring(10,14));
 
         //(i) 604 713 8180   
+	System.out.println();
 	
-	System.out.println(BCSS.substring(1,4)+" ");
+	System.out.print(BCSS.substring(1,4)+" ");
 	System.out.print(BCSS.substring(6,9)+" ");
 	System.out.print(BCSS.substring(10,14));
-        //(j) 604-713-8180     
+        //(j) 604-713-8180 
+	System.out.println();    
 
-	System.out.println(BCSS.substring(1,4));
-	System.out.print(BCSS.substring(6,9)+"-");
-	System.out.print(BCSS.substring(10,14)+"-");	
+	System.out.print(BCSS.substring(1,4));
+	System.out.print(BCSS.charAt(9));
+	System.out.print(BCSS.substring(6,9));
+	System.out.print(BCSS.charAt(9));
+	System.out.print(BCSS.substring(10,14));	
         //(k) 0818 317 406   
+	System.out.println();
+		     //0123456789abcd
+		     //(604) 713-8180
 
-	System.out.println(BCSS.substring(14,10));
-	System.out.print(BCSS.substring(9,6));
-	System.out.print(BCSS.substring(4,1));	
+
+	for(int i = 13; i>=0; i--){
+		if(i == 4 || i ==0)
+			System.out.print("");
+		else if(i == 9)
+			System.out.print(" ");
+		else
+			System.out.print(BCSS.charAt(i));
+	}
+	System.out.println();	
     }   
 
 }   
